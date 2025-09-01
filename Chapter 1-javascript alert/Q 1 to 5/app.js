a28b165f6786fe585ad91985e7eb73a279ab1c73
bhirawan.malik <bhirawanmalik000@gmail.com>
2025-09-01 13:39:14 +0500

    completed

---
+++ Chapter 1-javascript alert/Q 1 to 5/app.js
@@ -0,0 +1,26 @@
+// Q 1.Write a script to greet your website visitors using JS alert box.
+
+
+alert("Welcome to our website!");
+
+// Q 2.Write a script to display following message on your web page:
+
+alert("Error!Please enter a valid password.");
+
+// Q 3.Write a script to display following message to your web page: (Hint : Use line break)
+
+
+alert("Welcome to JS Land.... \n Happy Coding!");
+
+
+// Q 4.Write a script to display following message in sequence:
+// First alert
+
+alert("Welcome to JS Land....");
+
+// Second alert
+
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+// Q 5.Generate the following message through brower's developer console:
+alert("Hello...I can run JS through my web browser's console.");
