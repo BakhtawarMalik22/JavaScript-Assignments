910df656880b22dfae93da4d4d4873370885e100
bhirawan.malik <bhirawanmalik000@gmail.com>
2025-09-04 14:59:06 +0500

    work done

---
+++ Assignment 2/app.js
@@ -0,0 +1,58 @@
+// Question # 01
+
+var username;
+
+// Question # 02
+
+var myName = "Bukhtawar Batool"
+
+// Question # 03
+
+var message;
+
+var message = "Hello World"
+
+alert(message);
+
+// Question # 04
+
+var studentName = "Jhone Doe"
+
+var studentAge = "15 years old"
+
+var studentCourse = "Certified Mobile Application Development"
+
+
+// alerts
+
+alert(studentName);
+
+alert(studentAge);
+
+alert(studentCourse);
+
+// Question # 05
+
+var pizza = "    PIZZA \n PIZZ \n PIZ \n PI \n P"
+
+// Question # 06
+
+var email = "example@example.com"
+
+alert("My email address is"+" "+email);
+
+// Question # 07
+
+var book = "A smarter way to learn JavaScript"
+
+alert("I am trying to learn from the Book"+" "+book);
+
+// Question # 08
+
+document.write("Yah!I can write HTML content through JavaScript")
+
+// Question # 09
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+alert(design);
