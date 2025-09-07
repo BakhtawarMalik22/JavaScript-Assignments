88bbb285daa88da13253192a43c201ff7f4dcd77
bhirawan.malik <bhirawanmalik000@gmail.com>
2025-09-07 08:27:10 +0500

    completed

---
+++ Assignment 4/app.js
@@ -0,0 +1,57 @@
+// Question # 01
+
+// let studentName;
+
+// let studentAge;
+
+// let studentId;
+
+// Question # 02
+// Legal Variable
+
+// let myName;
+
+// let _age;
+
+// let $salary;
+
+// let user1;
+
+// let first_name;
+
+// Illegal Variable
+
+// let 1user;
+
+// let var;
+
+// let user-name;
+
+// let my name;
+
+// let @email ;
+
+
+// Question # 03
+
+document.write("<h1> Rules for naming JS Variables")
+
+document.write("<br />")
+
+document.write(" <p> Variable names can only contain Letters,Numbers,$,_.")
+
+document.write("For example $my,_1st Variable")
+
+document.write("<br />")
+
+document.write("Variable must begin with a Letter,$,_.")
+ 
+document.write("For example $name,_name or name")
+
+document.write("<br />");
+
+document.write("Variable names are case Sensitive.")
+
+document.write("<br />");
+
+document.write("Variable names should not be JS Keywords.")
